d5390ee7a51d1a27d872d59f20398be3e06018a5
empower87 <mjamespower87@gmail.com>
2022-01-11 23:06:04 -0800

    upgraded npm fro real

--- frontend/src/components/Feed.js
+++ frontend/src/components/Feed.js
@@ -1,16 +1,10 @@
-import { useContext, useState, useEffect, useRef, useCallback } from 'react'
+import { useState, useEffect, useRef, useCallback } from 'react'
 import { v4 as uuidv4 } from "uuid";
-// import TheViewContext from '../../TheViewContext'
-// import { songData } from './SongData'
 import Loading from './Loading'
-// import useDebugInformation from "../../utils/useDebugInformation"
 import gradientbg from '../images/gradient-bg-2.png'
 
 
 export default function HomeFeed({ feedSongs, setSongInView }) {
-  // useDebugInformation("IntersectionTest", props)
-//   const { setSongInView } = useContext(TheViewContext)
-//   const { homeFeedSongs } = useContext(songData)
   const [feedSongsCopy, setFeedSongsCopy] = useState([]);
   const [displayElements, setDisplayElements] = useState([])
   const viewRef = useRef();
@@ -47,9 +41,7 @@ export default function HomeFeed({ feedSongs, setSongInView }) {
           ref={setRefs}
           className="video-pane"
           key={`${uuidv4()}_${each.song._id}`}
-          style={{
-            backgroundImage: `url('${gradientbg}'), url('')`,
-          }}
+          style={{ backgroundImage: `url('${gradientbg}'), url('')` }}
         >
           <div className="last-div">
             {each.song.lyrics.map((each, index) => {
